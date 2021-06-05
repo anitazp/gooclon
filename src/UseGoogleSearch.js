0d5dd17007b9996f8825fd3ef0c5fa9d1fdb2b46
[anitazp] <[aalmanzap6@gmail.com]>
2021-06-04 20:57:36 -0500

    sin fallas

--- src/UseGoogleSearch.js
+++ src/UseGoogleSearch.js
@@ -10,16 +10,19 @@ const useGoogleSearch = (textString) => {
     const [data, setData] = useState(null);
     
     useEffect(() => {
+     if (textString) {
+       
       const URL = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${textString}`
       const fetchData = () =>{
           fetch(URL).then(response=>response.json().then(result=>setData(result)))
       }
       fetchData();
+     }
       
 
     }, [textString]);
 
 
-    return{data};
+    return {data} ;
 }
 export default useGoogleSearch;
